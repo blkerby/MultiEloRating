b6ed40013e3d58d0763d194cc32fdf120be70262
Brent Kerby <blkerby@gmail.com>
2025-09-30 21:30:53 -0600

    more experimentation and write-up

--- multi_elo_rating.py
+++ multi_elo_rating.py
@@ -42,6 +42,21 @@ def plackett_luce_gradient(ratings: list[float], inverted: bool) -> list[float]:
     return out.tolist()
 
 
+def plackett_luce_gradient_with_ranks(ratings: list[float], ranks: list[float]) -> list[float]:
+    ratings = torch.tensor(ratings, dtype=torch.double, requires_grad=True)
+    ranks = torch.tensor(ranks, dtype=torch.int64)
+    sorted_ranks, indices = torch.sort(ranks, descending=True)
+    num_censored = torch.sum(sorted_ranks == sorted_ranks[0])
+    assert num_censored < len(ratings)
+    ordered_ratings = ratings[indices]
+    
+    clam0 = torch.flip(torch.logcumsumexp(torch.flip(-ordered_ratings, [0]), 0), [0])
+    a = clam0[num_censored]
+    clam = torch.cat([torch.logaddexp(-ordered_ratings[:num_censored], a), clam0[num_censored:]], 0)
+    L = torch.sum(-ordered_ratings) - torch.sum(clam)
+    L.backward()
+    return ratings.grad.tolist()
+
 class Spline:
     def __init__(self, n, a, b, dtype=torch.double):
         super().__init__()
@@ -77,13 +92,13 @@ class Spline:
         y1 = torch.clamp_min(y1, 1e-30)
         log_y1 = torch.log(y1) + c[0]
         out = torch.cat([
-            torch.full_like(log_y1[0:1], float('-inf')),
+            torch.full_like(log_y1[0:1], -1e30),  # avoid -inf since it results in NaN gradients
             log_y1
         ], dim=0)
         return torch.logcumsumexp(out, dim=0) + math.log(self.scale)
 
 
-def get_rating_log_prob(ratings, num_censored, log_density_fn, margin):
+def get_rating_log_prob(ratings, weights, num_censored, log_density_fn, margin):
     # the +/-margin is to approximately cover the range on which the density functions
     # are non-negligibly different from zero:
     a = min(ratings).detach() - margin
@@ -98,7 +113,7 @@ def get_rating_log_prob(ratings, num_censored, log_density_fn, margin):
     i = 0
     F = 0
     while i < num_censored:
-        F = F + sp.log_antideriv(log_density_fn(x - ratings[i]))
+        F = F + weights[i] * sp.log_antideriv(log_density_fn(x - ratings[i]))
         i += 1
 
     while i < len(ratings):
@@ -114,15 +129,38 @@ def gaussian_log_density(x):
     return -torch.square(x) / 2
 
 
+def skew_gaussian_log_density(alpha):
+    def f(x):
+        return -torch.square(x) / 2 + torch.special.log_ndtr(alpha * x)
+    return f
+
+
+def logerfc(x): 
+    return torch.where(x > 0.0,
+                       torch.log(torch.special.erfcx(x)) - x**2,
+                       torch.log(torch.special.erfc(x)))
+
+def exp_modified_gaussian_log_density(lam):
+    def f(x):
+        return lam * x + logerfc((lam + x) / math.sqrt(2))
+    return f
+
+
 def hyperbolic_secant_density(x):
     return -torch.logaddexp(x, -x)
+
+
+def skew_hyperbolic_secant_density(alpha):
+    def f(x):
+        return -torch.logaddexp(x * alpha, -x)
+    return f
 
 
 def hyperbolic_exp_density(x):    
     return -torch.sqrt(torch.square(x) + 1)
 
 
-def get_thurstonian_rating_gradient(ratings: list[float], ranks: list[int], log_density_fn, margin: float) -> list[float]:
+def get_thurstonian_rating_gradient(ratings: list[float], ranks: list[int], weights: list[float], log_density_fn, margin: float) -> list[float]:
     """
     Given a list of player ratings and a list of corresponding ranks on a given game,
     return a list of gradients of the log-likelihood with respect to those ratings.
@@ -131,35 +169,56 @@ def get_thurstonian_rating_gradient(ratings: list[float], ranks: list[int], log_
     :param ratings: A list of player ratings.
     :param ranks: A corresponding list of player ranks in the outcome of the game. 
       Ties are supported only for last place (i.e. highest-numbered rank).
+    :param weights: Only supported for last place (i.e. highest-numbered rank).
     :param log_density_fn: Log of the density function to be used.
     :param margin: Value such that [-margin, margin] approximately captures the support of the density function.
     """
     ratings = torch.tensor(ratings, dtype=torch.double, requires_grad=True)
+    weights = torch.tensor(weights, dtype=torch.double)
     ranks = torch.tensor(ranks, dtype=torch.int64)
     sorted_ranks, indices = torch.sort(ranks, descending=True)
     num_censored = torch.sum(sorted_ranks == sorted_ranks[0])
     ordered_ratings = ratings[indices]
-    log_prob = get_rating_log_prob(ordered_ratings, num_censored, log_density_fn, margin)
+    ordered_weights = weights[indices]
+    log_prob = get_rating_log_prob(ordered_ratings, ordered_weights, num_censored, log_density_fn, margin)
     log_prob.backward()
     return ratings.grad.tolist()
   
   
-def get_updated_ratings(
+
+def get_updated_ratings_simple(
     ratings: list[float],
     ratings_grad: list[float],
-    rating_floor: float,
-    learning_rate_base: float,
-    learning_rate_decay: float,
-    negative_scaling_base: float,
-    negative_scaling_decay: float,
+    learning_rate: float,
 ) -> list[float]:
     """
     Given a list of player ratings and a list of corresponding ranks on a given game,
     return a list of updated player ratings.
     :param ratings: A list of player ratings.
     :param ratings_grad: A list of player rating gradient values.
-    :param ranks: A corresponding list of player ranks in the outcome of the game. 
-      Ties are supported only for last place (i.e. highest-numbered rank).
+    :param learning_rate: the learning rate (a.k.a. step size).
+    :return: A list of updated player ratings.
+     """
+    out = []
+    for i in range(len(ratings)):
+        old_rating = ratings[i]
+        grad = ratings_grad[i]
+        new_rating = old_rating + learning_rate * grad
+        out.append(new_rating)
+    return out
+
+def get_updated_ratings(
+    ratings: list[float],
+    ratings_grad: list[float],
+    rating_floor: float,
+    knots: list[float],
+    learning_rates: list[float],
+) -> list[float]:
+    """
+    Given a list of player ratings and a list of corresponding ranks on a given game,
+    return a list of updated player ratings.
+    :param ratings: A list of player ratings.
+    :param ratings_grad: A list of player rating gradient values.
     :param rating_floor: the minimum possible rating; any rating that drops below this will be clamped to this value.
     :param learning_rate_base: the maximum learning rate (a.k.a. step size), applied for ratings at the rating floor.
     :param learning_rate_decay: the rate at which the learning rate decays exponentially as a function of the rating.
@@ -175,9 +234,7 @@ def get_updated_ratings(
     for i in range(len(ratings)):
         old_rating = ratings[i]
         grad = ratings_grad[i]
-        lr = learning_rate_base * math.exp(-(old_rating - rating_floor) * learning_rate_decay)
-        if grad < 0:
-            grad *= 1 - negative_scaling_base * math.exp(-(old_rating - rating_floor) * negative_scaling_decay)
+        lr = np.interp(old_rating, knots, learning_rates)
         new_rating = old_rating + lr * grad
         if new_rating < rating_floor:
             new_rating = rating_floor
--- racetime_analysis/model_analysis.py
+++ racetime_analysis/model_analysis.py
@@ -11,8 +11,15 @@ import trueskill
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import torch
 
+# torch.autograd.set_detect_anomaly(True)
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+# x = torch.arange(-5, 5, 0.01)
+# y = torch.exp(multi_elo_rating.exp_modified_gaussian_log_density(1)(x))
+# plt.plot(x, y)
+# plt.show()
 
 parser = argparse.ArgumentParser(
     prog='model_analysis',
@@ -20,30 +27,44 @@ parser = argparse.ArgumentParser(
 parser.add_argument("goal", help="Racetime goal (e.g., Map Rando S4)")
 parser.add_argument("data_dir", help="Directory to load data from")
 parser.add_argument("--trueskill", action="store_true")
+parser.add_argument("--dummy", action="store_true", help="Include dummy player")
+parser.add_argument("--baseline", action="store_true")
 args = parser.parse_args()
 
 data_path = os.path.join(args.data_dir, f"{args.goal}.json")
 race_list = json.load(open(data_path, "r"))
 
 # Filter to recorded races:
-# race_list = [race for race in race_list if race["recorded"]]
+race_list = [race for race in race_list if race["recorded"]]
 
 # trueskill_env = trueskill.TrueSkill(backend='mpmath')
-# trueskill_env = trueskill.TrueSkill(backend=None, mu=0.0, sigma=3.0, beta=3.5, tau=0.8, draw_probability=1e-3)
-trueskill_env = trueskill.TrueSkill(backend=None, mu=0.0, sigma=8.0, beta=5.5, tau=0.4, draw_probability=0.01)
+trueskill_env = trueskill.TrueSkill(backend=None, sigma=5.0, beta=2.5, tau=0.12, draw_probability=1e-3)
+# trueskill_env = trueskill.TrueSkill(backend=None, sigma=3.0, beta=2.0, tau=0.12, draw_probability=1e-3)
+# trueskill_env = trueskill.TrueSkill(backend=None, sigma=3.0, beta=2.0, tau=0.12, draw_probability=1e-3)
+# trueskill_env = trueskill.TrueSkill(backend=None, sigma=3.7, beta=2.3, tau=0.18, draw_probability=1e-3)
+# trueskill_env = trueskill.TrueSkill(backend=None, sigma=3.6, beta=3.0, tau=0.15, draw_probability=1e-5)
+
+# trueskill_env = trueskill.TrueSkill(backend=None, mu=0.0, sigma=8.0, beta=5.5, tau=0.4, draw_probability=0.01)
 
 user_scores = {}
 user_scores_history = defaultdict(lambda: [])
 
-average_lr = 0.002
-# average_weight = 0.02
-average_weight = 0.0
-average_score = 0.0
+dummy_player_weight = 1.0
 
-# default_lr = 0.1
-default_lr = 0.0
-default_quantile = 0.09
-default_score = 0.0
+# Optimized values for TrueSkill:
+default_score = 25
+dummy_sigma = 0.01
+dummy_score = 21
+rating_floor = -float("inf")
+
+# Optimized values for Thurstonian model:
+# default_score = 0.1
+# dummy_score = -0.1
+# rating_floor = 0.0
+
+# rating_floor = 0.0
+# default_score = 0.25
+# dummy_score = 1.35
 
 # Evaluate using kendall-tau correlation:
 def eval_race(entrants):
@@ -61,7 +82,8 @@ def eval_race(entrants):
             continue
         score1 = user_scores.get(user1, default_rating)
         if args.trueskill:
-            score1 = score1.mu
+            score1 = score1.mu - 2 * score1.sigma
+            # score1 = score1.mu
         for j in range(i + 1, len(entrants)):
             e2 = entrants[j]
             user2 = e2["user_id"]
@@ -69,10 +91,11 @@ def eval_race(entrants):
                 continue
             score2 = user_scores.get(user2, default_rating)
             if args.trueskill:
-                score2 = score2.mu
+                score2 = score2.mu - 2 * score2.sigma
+                # score2 = score2.mu
             if e1["place"] == e2["place"]:
                 continue
-            if score1 == score2:
+            if score1 == score2 or np.isnan(score1) or np.isnan(score2):
                 cnt_incorrect += 0.5
             else:
                 incorrect = (e1["place"] < e2["place"]) == (score1 < score2)
@@ -82,10 +105,6 @@ def eval_race(entrants):
     return cnt_incorrect, cnt_pair
 
 def score_race(race_idx, entrants):
-    # TODO: move these out of globals
-    global average_score
-    global default_score
-    
     if args.trueskill:
         default_rating = trueskill_env.create_rating(mu=default_score)
     else:
@@ -94,7 +113,9 @@ def score_race(race_idx, entrants):
     user_id_list = []
     rating_list = []
     rank_list = []
-    for e in entrants:
+    weight_list = []
+    entrant_idx_list = []
+    for i, e in enumerate(entrants):
         user_id = e["user_id"]
         if user_id is None:
             continue
@@ -103,143 +124,251 @@ def score_race(race_idx, entrants):
         user_id_list.append(user_id)
         rating_list.append(rating)
         rank_list.append(rank)
+        weight_list.append(1)
+        entrant_idx_list.append(i)
+
+    if args.dummy:
+        # Add a dummy player tied for last place, with a constant rating, to anchor the rating scale:
+        if args.trueskill:
+            rating_list.append(trueskill.Rating(mu=dummy_score, sigma=dummy_sigma))
+        else:
+            rating_list.append(dummy_score)
+        rank_list.append(entrants[-1]["place"])
+        weight_list.append(dummy_player_weight)
 
     if args.trueskill:
-        rating_list = [(x,) for x in rating_list]
-        new_ratings = trueskill_env.rate(rating_list, rank_list)
+        wrapped_rating_list = [(x,) for x in rating_list]
+        new_ratings = trueskill_env.rate(wrapped_rating_list, rank_list)
         new_ratings = [x[0] for x in new_ratings]
+        for i in range(len(new_ratings)):
+            new_ratings[i] = trueskill.Rating(mu=max(rating_floor, new_ratings[i].mu), sigma=new_ratings[i].sigma)
     else:                  
-        # ratings_grad = multi_elo_rating.plackett_luce_gradient(
-        #     rating_list,
-        #     inverted=False)
-        # new_ratings = multi_elo_rating.get_updated_ratings(
-        #     rating_list,
-        #     ratings_grad,
-        #     rating_floor=-0.4,
-        #     learning_rate_base=0.85,
-        #     learning_rate_decay=0.8,
-        #     negative_scaling_base=1.0,
-        #     negative_scaling_decay=0.45,
-        # )
-
-        # ratings_grad = multi_elo_rating.plackett_luce_gradient(
-        #     rating_list,
-        #     inverted=True)
-        # new_ratings = multi_elo_rating.get_updated_ratings(
-        #     rating_list,
-        #     ratings_grad,
-        #     rating_floor=-0.4,
-        #     learning_rate_base=1.2,
-        #     learning_rate_decay=0.9,
-        #     negative_scaling_base=1.0,
-        #     negative_scaling_decay=0.45,
-        # )
-
+        ######## Constant learning rate, no dummy player:
+    
         # ratings_grad = multi_elo_rating.pairwise_elo_gradient(
         #     rating_list,
         #     rank_list,
         #     use_average=False)
-        # new_ratings = multi_elo_rating.get_updated_ratings(
+        # new_ratings = multi_elo_rating.get_updated_ratings_simple(
         #     rating_list,
         #     ratings_grad,
-        #     rating_floor=-0.4,
-        #     learning_rate_base=0.18,
-        #     learning_rate_decay=0.7,
-        #     negative_scaling_base=0.9,
-        #     negative_scaling_decay=0.4,
+        #     learning_rate=0.07,
         # )
 
         # ratings_grad = multi_elo_rating.pairwise_elo_gradient(
         #     rating_list,
         #     rank_list,
         #     use_average=True)
-        # new_ratings = multi_elo_rating.get_updated_ratings(
+        # new_ratings = multi_elo_rating.get_updated_ratings_simple(
         #     rating_list,
         #     ratings_grad,
-        #     rating_floor=-0.05,
-        #     learning_rate_base=4.0,
-        #     learning_rate_decay=0.9,
-        #     negative_scaling_base=0.8,
-        #     negative_scaling_decay=0.45,
+        #     learning_rate=0.75,
+        # )
+        
+        # ratings_grad = multi_elo_rating.plackett_luce_gradient_with_ranks(
+        #     rating_list,
+        #     rank_list)
+        # new_ratings = multi_elo_rating.get_updated_ratings_simple(
+        #     rating_list,
+        #     ratings_grad,
+        #     learning_rate=0.32,
         # )
 
-        ratings_grad = multi_elo_rating.get_thurstonian_rating_gradient(
+        # ratings_grad = multi_elo_rating.get_thurstonian_rating_gradient(
+        #     rating_list,
+        #     rank_list,
+        #     weight_list,
+        #     log_density_fn=multi_elo_rating.gaussian_log_density,
+        #     margin=5)
+        # new_ratings = multi_elo_rating.get_updated_ratings_simple(
+        #     rating_list,
+        #     ratings_grad,
+        #     learning_rate=0.26,
+        # )
+
+
+        ######## Constant learning rate, with dummy player:
+    
+        # ratings_grad = multi_elo_rating.pairwise_elo_gradient(
+        #     rating_list,
+        #     rank_list,
+        #     use_average=False)
+        # new_ratings = multi_elo_rating.get_updated_ratings_simple(
+        #     rating_list,
+        #     ratings_grad,
+        #     learning_rate=0.07,
+        # )
+
+        # ratings_grad = multi_elo_rating.pairwise_elo_gradient(
+        #     rating_list,
+        #     rank_list,
+        #     use_average=True)
+        # new_ratings = multi_elo_rating.get_updated_ratings_simple(
+        #     rating_list,
+        #     ratings_grad,
+        #     learning_rate=0.63,
+        # )
+        
+        # ratings_grad = multi_elo_rating.plackett_luce_gradient_with_ranks(
+        #     rating_list,
+        #     rank_list)
+        # new_ratings = multi_elo_rating.get_updated_ratings_simple(
+        #     rating_list,
+        #     ratings_grad,
+        #     learning_rate=0.18,
+        # )
+
+        # ratings_grad = multi_elo_rating.get_thurstonian_rating_gradient(
+        #     rating_list,
+        #     rank_list,
+        #     weight_list,
+        #     log_density_fn=multi_elo_rating.gaussian_log_density,
+        #     margin=5)
+        # new_ratings = multi_elo_rating.get_updated_ratings_simple(
+        #     rating_list,
+        #     ratings_grad,
+        #     learning_rate=0.11,
+        # )
+
+
+        ####### Variable learning rate, with rating floor:
+        ratings_grad = multi_elo_rating.plackett_luce_gradient_with_ranks(
             rating_list,
-            rank_list,
-            log_density_fn=multi_elo_rating.gaussian_log_density,
-            margin=5)
+            rank_list)
         new_ratings = multi_elo_rating.get_updated_ratings(
             rating_list,
             ratings_grad,
-            rating_floor=-0.05,
-            learning_rate_base=0.9,
-            learning_rate_decay=1.2,
-            negative_scaling_base=0.8,
-            negative_scaling_decay=0.45,
+            rating_floor=rating_floor,
+            knots=[0.0, 1.0, 2.0],
+            learning_rates=[0.6, 0.13, 0.09],
         )
 
         # ratings_grad = multi_elo_rating.get_thurstonian_rating_gradient(
         #     rating_list,
         #     rank_list,
+        #     weight_list,
+        #     log_density_fn=multi_elo_rating.gaussian_log_density,
+        #     margin=5)
+        # new_ratings = multi_elo_rating.get_updated_ratings(
+        #     rating_list,
+        #     ratings_grad,
+        #     rating_floor=rating_floor,
+        #     knots=[0.0, 1.0, 2.0],
+        #     learning_rates=[0.65, 0.09, 0.07],
+        # )
+
+        # ratings_grad = multi_elo_rating.get_thurstonian_rating_gradient(
+        #     rating_list,
+        #     rank_list,
+        #     weight_list,
+        #     log_density_fn=multi_elo_rating.skew_gaussian_log_density(-1.0),
+        #     margin=5)
+        # new_ratings = multi_elo_rating.get_updated_ratings(
+        #     rating_list,
+        #     ratings_grad,
+        #     rating_floor=rating_floor,
+        #     knots=[0.0, 1.0, 2.0],
+        #     learning_rates=[0.7, 0.07, 0.07],
+        # )
+
+        # ratings_grad = multi_elo_rating.get_thurstonian_rating_gradient(
+        #     rating_list,
+        #     rank_list,
+        #     weight_list,
+        #     log_density_fn=multi_elo_rating.exp_modified_gaussian_log_density(1.0),
+        #     margin=10)
+        # new_ratings = multi_elo_rating.get_updated_ratings(
+        #     rating_list,
+        #     ratings_grad,
+        #     rating_floor=0,
+        #     learning_rate_base=0.95,
+        #     learning_rate_decay=0.6,
+        #     negative_scaling_base=0.0,
+        #     negative_scaling_decay=0.6,
+        # )
+        # new_ratings = multi_elo_rating.get_updated_ratings2(
+        #     rating_list,
+        #     ratings_grad,
+        #     rating_floor=rating_floor,
+        #     knots=[0.0, 1.0, 2.0, 3.0],
+        #     learning_rates=[0.95, 0.5, 0.35, 0.1],
+        # )
+
+        # ratings_grad = multi_elo_rating.get_thurstonian_rating_gradient(
+        #     rating_list,
+        #     rank_list,
+        #     weight_list,
         #     log_density_fn=multi_elo_rating.hyperbolic_exp_density,
         #     margin=10)
         # new_ratings = multi_elo_rating.get_updated_ratings(
         #     rating_list,
         #     ratings_grad,
-        #     rating_floor=-0.05,
-        #     learning_rate_base=2.0,
-        #     learning_rate_decay=1.05,
-        #     negative_scaling_base=0.8,
+        #     rating_floor=0.0,
+        #     learning_rate_base=0.95,
+        #     learning_rate_decay=0.4,
+        #     negative_scaling_base=0.0,
         #     negative_scaling_decay=0.45,
         # )
 
         # ratings_grad = multi_elo_rating.get_thurstonian_rating_gradient(
         #     rating_list,
         #     rank_list,
+        #     weight_list,
         #     log_density_fn=multi_elo_rating.hyperbolic_secant_density,
         #     margin=10)
         # new_ratings = multi_elo_rating.get_updated_ratings(
         #     rating_list,
         #     ratings_grad,
-        #     rating_floor=-0.05,
-        #     learning_rate_base=2.0,
-        #     learning_rate_decay=1.05,
-        #     negative_scaling_base=0.8,
+        #     rating_floor=0.0,
+        #     learning_rate_base=1.5,
+        #     learning_rate_decay=0.8,
+        #     negative_scaling_base=0.0,
         #     negative_scaling_decay=0.45,
         # )
 
-    if args.trueskill:
-        for i in range(len(new_ratings)):
-            adjusted_mu = new_ratings[i].mu - average_score * average_weight
-            new_ratings[i] = trueskill.Rating(mu=adjusted_mu, sigma=new_ratings[i].sigma)
-    else:
-        for i in range(len(new_ratings)):
-            adjusted_mu = new_ratings[i] - average_score * average_weight
-            new_ratings[i] = adjusted_mu
-
+    # if args.trueskill:
+    #     for i in range(len(new_ratings)):
+    #         adjusted_mu = new_ratings[i].mu - average_score * average_weight
+    #         new_ratings[i] = trueskill.Rating(mu=adjusted_mu, sigma=new_ratings[i].sigma)
+    # else:
+    #     for i in range(len(new_ratings)):
+    #         adjusted_mu = new_ratings[i] - average_score * average_weight
+    #         new_ratings[i] = adjusted_mu
     for i in range(len(user_id_list)):
         user_id = user_id_list[i]
+        old_rating = rating_list[i]
         new_rating = new_ratings[i]
+        entrant = entrants[entrant_idx_list[i]]
+        # if entrant["place"] == entrants[-1]["place"] and new_rating.mu > old_rating.mu:
+        #     # print("last up: ", old_rating, new_rating, rank_list, rating_list, new_ratings)
+        #     print(len(list(e for e in entrants if e["place"] == entrants[-1]["place"])))
+        # if entrant["place"] == entrants[0]["place"] and new_rating < old_rating:
+        #     print("first down")
+        # if entrant["place"] == entrants[-1]["place"] and new_rating > old_rating:
+        #     print(len(list(e for e in entrants if e["place"] == entrants[-1]["place"])))
+        # if entrant["place"] == entrants[-1]["place"] and entrant["score_change"] is not None and entrant["score_change"] > 0:
+        #     print(len(list(e for e in entrants if e["place"] == entrants[-1]["place"])))
+        if args.baseline:
+            new_rating = entrant["score"]
+            if new_rating is not None and entrant["score_change"] is not None:
+                new_rating += entrant["score_change"]
+            else:
+                continue
         user_scores[user_id] = new_rating
         user_scores_history[user_id].append((race_idx, new_rating))
         
-    if args.trueskill:
-        race_average_score = sum(r.mu for r in new_ratings) / len(new_ratings)
-    else:
-        race_average_score = sum(new_ratings) / len(new_ratings)
-    average_score = (1 - average_lr) * average_score + average_lr * race_average_score
-    
-    if args.trueskill:
-        cnt_above = sum(1 for r in new_ratings if r.mu > default_score)
-    else:
-        cnt_above = sum(1 for r in new_ratings if r > default_score)        
-    cnt_below = len(new_ratings) - cnt_above
-    default_score += default_lr * (cnt_above * default_quantile - cnt_below * (1 - default_quantile))
-    
     # print(cnt_above, cnt_below, len(new_ratings), average_score, default_score)
 
 eval_pairs = []
 for race_idx, race in enumerate(race_list):
+    # winner_change = race["entrants"][0]["score_change"]
+    # winner_change = race["entrants"][-1]["score_change"]
+    # for e in race["entrants"]:
+    #     if e["place"] == race["entrants"][0]["place"] and e["score_change"] < 0:
+    #         print("first:", e["score_change"])
+    #     if e["place"] == race["entrants"][-1]["place"] and e["score_change"] > 0:
+    #         print("last:", e["score_change"])
+
     cnt_incorrect, cnt_pairs = eval_race(race["entrants"])
     if cnt_pairs > 0:
         eval_pairs.append((cnt_incorrect, cnt_pairs))
@@ -257,17 +386,36 @@ histories.sort(key=lambda x: len(x[1]), reverse=True)
 # print(list((x[0], len(x[1])) for x in histories))
 # print(histories[0])
 
+user_cnt5 = 0
+user_win5 = 0
+if args.trueskill:
+    initial_score = default_score - 2 * trueskill_env.sigma
+else:
+    initial_score = default_score
+for user_id, h in histories:
+    if len(h) >= 5:
+        user_cnt5 += 1
+        race_idx, score = h[4]
+        if args.trueskill:
+            # score = score.mu
+            score = score.mu - 2 * score.sigma
+        if score > initial_score:
+            user_win5 += 1
+print("user_cnt5 = {}, user_win5={}, user_rate5={}".format(user_cnt5, user_win5, user_win5 / user_cnt5))
+
 user_id_list = []
 time_list = []
 score_list = []
-for user_id, h in histories:
+for user_id, h in histories[:15]:
     for race_idx, score in h:
         user_id_list.append(user_id)
         # t = datetime.datetime.fromisoformat(race_list[race_idx]["ended_at"].replace('Z', '+00:00'))
         # time_list.append(t)
         time_list.append(race_idx)
         if args.trueskill:
-            score_list.append(score.mu)
+            # score_list.append(score.mu * 100)
+            score_list.append((score.mu - 2 * score.sigma) * 100)
+            # score_list.append(score.sigma)
         else:
             score_list.append(score)
 
@@ -275,8 +423,9 @@ print("kendall_tau: {}".format(kt))
 df = pd.DataFrame({
     "User ID": user_id_list,
     "Race": time_list,
-    "Simulated Rating": score_list})
+    "Rating": score_list})
+
 
 sns.set_theme()
-sns.relplot(df, kind="line", x="Race", y="Simulated Rating", hue="User ID")
+sns.relplot(df, kind="line", x="Race", y="Rating", hue="User ID")
 plt.show()
